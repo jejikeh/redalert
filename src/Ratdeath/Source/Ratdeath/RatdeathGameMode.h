f2762c716db3c67b7fb1037e52fb3a3a1de64562
jejikeh <jejikeh@gmail.com>
2023-09-19 14:58:34 +0300

    update gitignores, simple WIP project structure

---
+++ src/Ratdeath/Source/Ratdeath/RatdeathGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RatdeathGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARatdeathGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARatdeathGameMode();
+};
+
+
+
